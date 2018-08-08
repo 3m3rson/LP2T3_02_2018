57dfd6c407acc24438d0a6cbbfb9e005c9b15528
Lucas Oliveira <lucas.oliveira@ifsp.edu.br>
2018-08-07 23:49:16 -0300

    Exercícios feitos em sala nas aulas 1 e 2.

---
+++ Aula_01_Ex_01/Aula_01_Ex_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_01_Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palmeiras não tem mundial.");
+            String result = Console.ReadLine();
+
+            Console.WriteLine("Resultado {0} do que eu digitei {1}", result, "segundo numero");
+            Console.Read();
+        }
+    }
+}
---
+++ Aula_02_Ex_02/Aula_02_Ex_02/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_02_Ex_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Primeiro numero: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Segundo numero: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Terceiro numero: ");
+            c = int.Parse(Console.ReadLine());
+
+            if(a > b && a > c)
+            {
+                if(b > c)
+                    Console.Write("{0}, {1}, {2}", c, b, a);
+                else
+                    Console.Write("{0}, {1}, {2}", b, c, a);
+            }
+            if (b > a && b > c)
+            {
+                if (a > c)
+                    Console.Write("{0}, {1}, {2}", c, a, b);
+                else
+                    Console.Write("{0}, {1}, {2}", a, c, b);
+            }
+
+            if (c > a && c > b)
+            {
+                if (a > b)
+                    Console.Write("{0}, {1}, {2}", b, a, c);
+                else
+                    Console.Write("{0}, {1}, {2}", a, b, c);
+            }
+
+            Console.Read();
+        }
+    }
+}
