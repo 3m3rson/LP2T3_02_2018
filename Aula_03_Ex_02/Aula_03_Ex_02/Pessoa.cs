85007d46b5bab1f43e1a32c42ca374b44684f9a9
Lucas Oliveira <lucas.oliveira@ifsp.edu.br>
2018-08-28 22:04:17 -0300

    aula 04 e ex 02 da aula 03

---
+++ Aula_03_Ex_02/Aula_03_Ex_02/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_03_Ex_02
+{
+    class Pessoa
+    {
+        private Relacionamento namoro;
+        private String nome;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        internal Relacionamento Namoro
+        {
+            get
+            {
+                return namoro;
+            }
+
+            set
+            {
+                namoro = value;
+            }
+        }
+    }
+}
---
+++ Aula_04_Ex_01/Aula_04_Ex_01/Figura.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_04_Ex_01
+{
+    class Figura
+    {
+        private int x, y;
+
+        public Figura(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public override string ToString()
+        {
+            return "[" + X + ", " + Y + "]";
+        }
+
+        public virtual double Area()
+        {
+            return 0;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
+        
+    }
+}
